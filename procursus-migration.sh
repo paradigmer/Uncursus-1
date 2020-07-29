1489a44f326ff68cb9960de3250fb09cbadf9652
Yaya4 <34249303+Yaya48@users.noreply.github.com>
2020-07-30 01:36:55 +0200

    Update procursus-migration.sh

--- procursus-migration.sh
+++ procursus-migration.sh
@@ -34,6 +34,7 @@ cp /var/root/migration/libncurses.6.dylib /usr/lib
 apt full-upgrade -y --allow-unauthenticated -u -o APT::Force-LoopBreak=1
 dpkg -i --force-all /var/root/migration/coreutils_8.32-1_iphoneos-arm.deb
 dpkg -r apt1.4
+dpkg -r cydia-list
 rm -rf /etc/apt/sources.list.d/cydia.list
 echo "Types: deb" > /etc/apt/sources.list.d/procursus.sources
 echo "URIs: https://apt.procurs.us/" >> /etc/apt/sources.list.d/procursus.sources
