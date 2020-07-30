21278f7b207e85be1207b12feb47203c1c9431e4
Yaya4 <34249303+Yaya48@users.noreply.github.com>
2020-07-30 20:53:34 +0200

    Update installuncursus.sh

--- installuncursus.sh
+++ installuncursus.sh
@@ -3,7 +3,7 @@ if [ "$EUID" -ne 0 ]; then
 echo You need to run this script as root.
 else
 clear
-echo -e "\e[31mWelcome to Uncursus Installation Script V2.0.0 (Beta3) By @Yaya4_4 on Twitter.\e[0m"
+echo -e "\e[31mWelcome to Uncursus Installation Script V2.0.0 (Beta4) By @Yaya4_4 on Twitter.\e[0m"
 echo "Checking If Is A Clean Install Of Unc0ver..."
 if [[ -f "/.procursus_strapped" ]]; then
 echo "Please do not use this on odyssey or on an already uncursus installation.."
