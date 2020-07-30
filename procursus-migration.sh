2502c71178918c1014b948323a923c7e111d8f32
Yaya4 <34249303+Yaya48@users.noreply.github.com>
2020-07-30 03:41:36 +0200

    Update procursus-migration.sh

--- procursus-migration.sh
+++ procursus-migration.sh
@@ -3,7 +3,7 @@ if [ "$EUID" -ne 0 ]; then
 echo You need to run this script as root.
 else
 clear
-echo "Uncursus 2.0 Migration Part By Yaya4_4 1.0(Beta4)"
+echo "Uncursus 2.0 Migration Part By Yaya4_4 1.0(Beta5)"
 echo "Checking iOS Version"
 VER=$(/usr/bin/plutil -key ProductVersion /System/Library/CoreServices/SystemVersion.plist)
 if [[ "${VER%.*}" -ge 12 ]] && [[ "${VER%.*}" -lt 13 ]]; then
@@ -21,14 +21,14 @@ rm /etc/apt/sources.list.d/cydia.list
 echo "deb https://apt.procurs.us/ iphoneos-arm64/${CFVER} main" >> /etc/apt/sources.list.d/cydia.list
 rm -rf /var/root/migration
 mkdir /var/root/migration
-wget http://apt.procurs.us/pool/main/iphoneos-arm64/${CFVER}/procursus-keyring_2020.05.09_iphoneos-arm.deb --no-check-certificate --directory-prefix=/var/root/migration
-wget https://apt.procurs.us/pool/main/iphoneos-arm64/${CFVER}/coreutils_8.32-1_iphoneos-arm.deb --no-check-certificate --directory-prefix=/var/root/migration
-wget http://yaya48.gq/files/migration-files/elucubratustoprocursus/libncurses.6.dylib --no-check-certificate --directory-prefix=/var/root/migration
+wget -q http://apt.procurs.us/pool/main/iphoneos-arm64/${CFVER}/procursus-keyring_2020.05.09_iphoneos-arm.deb --no-check-certificate --directory-prefix=/var/root/migration
+wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/${CFVER}/coreutils_8.32-1_iphoneos-arm.deb --no-check-certificate --directory-prefix=/var/root/migration
+wget -q http://yaya48.gq/files/migration-files/elucubratustoprocursus/libncurses.6.dylib --no-check-certificate --directory-prefix=/var/root/migration
 dpkg -i /var/root/migration/procursus-keyring_2020.05.09_iphoneos-arm.deb
 apt update
 apt install xz-utils -y --allow-unauthenticated -u -o APT::Force-LoopBreak=1
-wget https://yaya48.gq/files/migration-files/shared/${CFVER}/liblzma.dylib --no-check-certificate --directory-prefix=/usr/local/lib
-wget https://yaya48.gq/files/migration-files/shared/${CFVER}/liblzma.5.dylib --no-check-certificate --directory-prefix=/usr/local/lib
+wget -q https://yaya48.gq/files/migration-files/shared/${CFVER}/liblzma.dylib --no-check-certificate --directory-prefix=/usr/local/lib
+wget -q https://yaya48.gq/files/migration-files/shared/${CFVER}/liblzma.5.dylib --no-check-certificate --directory-prefix=/usr/local/lib
 apt install xz-utils -y --allow-unauthenticated -u -o APT::Force-LoopBreak=1
 apt full-upgrade -y --allow-unauthenticated -u -o APT::Force-LoopBreak=1 
 cp /var/root/migration/libncurses.6.dylib /usr/lib
