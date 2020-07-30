b652d88e5145366319802ee9ac7786b9767b0b0a
Yaya4 <34249303+Yaya48@users.noreply.github.com>
2020-07-30 02:45:07 +0200

    Update procursus-migration.sh

--- procursus-migration.sh
+++ procursus-migration.sh
@@ -3,7 +3,7 @@ if [ "$EUID" -ne 0 ]; then
 echo You need to run this script as root.
 else
 clear
-echo "Uncursus 2.0 Migration Part By Yaya4_4 1.0(Beta3)"
+echo "Uncursus 2.0 Migration Part By Yaya4_4 1.0(Beta4)"
 echo "Checking iOS Version"
 VER=$(/usr/bin/plutil -key ProductVersion /System/Library/CoreServices/SystemVersion.plist)
 if [[ "${VER%.*}" -ge 12 ]] && [[ "${VER%.*}" -lt 13 ]]; then
@@ -19,6 +19,7 @@ apt update
 apt install wget -y --allow-unauthenticated
 rm /etc/apt/sources.list.d/cydia.list
 echo "deb https://apt.procurs.us/ iphoneos-arm64/${CFVER} main" >> /etc/apt/sources.list.d/cydia.list
+rm -rf /var/root/migration
 mkdir /var/root/migration
 wget http://apt.procurs.us/pool/main/iphoneos-arm64/${CFVER}/procursus-keyring_2020.05.09_iphoneos-arm.deb --no-check-certificate --directory-prefix=/var/root/migration
 wget https://apt.procurs.us/pool/main/iphoneos-arm64/${CFVER}/coreutils_8.32-1_iphoneos-arm.deb --no-check-certificate --directory-prefix=/var/root/migration
