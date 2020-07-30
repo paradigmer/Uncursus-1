56f6eb84240b6d9708807983ddc218a2b4453a85
Yaya4 <34249303+Yaya48@users.noreply.github.com>
2020-07-30 02:47:36 +0200

    Update procursus-migration.sh

--- procursus-migration.sh
+++ procursus-migration.sh
@@ -41,4 +41,5 @@ echo "Types: deb" > /etc/apt/sources.list.d/procursus.sources
 echo "URIs: https://apt.procurs.us/" >> /etc/apt/sources.list.d/procursus.sources
 echo "Suites: iphoneos-arm64/${CFVER}" >> /etc/apt/sources.list.d/procursus.sources
 echo "Components: main" >> /etc/apt/sources.list.d/procursus.sources
+rm -rf /var/root/migration
 fi
