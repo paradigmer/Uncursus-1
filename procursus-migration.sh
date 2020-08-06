822bc567dc723e1b3b0780018ffdbf800a6eec19
Yaya4 <34249303+Yaya48@users.noreply.github.com>
2020-08-06 18:25:02 +0200

    Update procursus-migration.sh

--- procursus-migration.sh
+++ procursus-migration.sh
@@ -4,7 +4,7 @@ echo You need to run this script as root.
 else
 clear
 echo "Copyright (c) 2020, Yaya4 All rights reserved."
-echo -e "\e[31mUncursus 2.0 Migration Part By Yaya4_4 1.0.2 (Sbtale)\e[0m"
+echo -e "\e[31mUncursus 2.0 Migration Part By Yaya4_4 1.0.3 (Sbtale)\e[0m"
 echo "Checking iOS Version"
 VER=$(/usr/bin/plutil -key ProductVersion /System/Library/CoreServices/SystemVersion.plist)
 if [[ "${VER%.*}" -ge 12 ]] && [[ "${VER%.*}" -lt 13 ]]; then
@@ -50,6 +50,10 @@ dpkg -i --force-all /var/root/migration/firmware-sbin_0-1_iphoneos-arm.deb
 apt full-upgrade -y --allow-unauthenticated -u -o APT::Force-LoopBreak=1
 dpkg -i --force-all /var/root/migration/coreutils_8.32-1_iphoneos-arm.deb
 dpkg -r apt1.4
+apt update
+apt purge libplist-utils -y libplist3 -y
+apt autoremove -y
+apt install libplist-utils -y libplist++-dev -y libplist++-dev -y libplist++3v5 -y libplist-dev -y libplist3 -y ldid -y
 echo "Types: deb" > /etc/apt/sources.list.d/procursus.sources
 echo "URIs: https://apt.procurs.us/" >> /etc/apt/sources.list.d/procursus.sources
 echo "Suites: iphoneos-arm64/${CFVER}" >> /etc/apt/sources.list.d/procursus.sources
