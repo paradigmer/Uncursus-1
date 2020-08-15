38c21358c11d81885d6f28261dabc65984f1741b
Yaya4 <34249303+Yaya48@users.noreply.github.com>
2020-08-16 01:05:50 +0200

    Change Temp Directory

--- procursus-migration.sh
+++ procursus-migration.sh
@@ -4,7 +4,7 @@ echo You need to run this script as root.
 else
 clear
 echo "Copyright (c) 2020, Yaya4 All rights reserved."
-echo -e "\e[31mUncursus 2.0 Migration Part By Yaya4_4 1.0.4.1 (Sbtale)\e[0m"
+echo -e "\e[31mUncursus 2.0 Migration Part By Yaya4_4 1.0.5 (Sbtale)\e[0m"
 echo "Checking iOS Version"
 VER=$(/usr/bin/plutil -key ProductVersion /System/Library/CoreServices/SystemVersion.plist)
 if [[ "${VER%.*}" -ge 12 ]] && [[ "${VER%.*}" -lt 13 ]]; then
@@ -32,23 +32,23 @@ apt update
 apt install wget -y --allow-unauthenticated
 rm /etc/apt/sources.list.d/cydia.list
 echo "deb https://apt.procurs.us/ iphoneos-arm64/${CFVER} main" >> /etc/apt/sources.list.d/cydia.list
-rm -rf /var/root/migration
-mkdir /var/root/migration
-wget -q http://apt.procurs.us/pool/main/iphoneos-arm64/${CFVER}/procursus-keyring_2020.05.09_iphoneos-arm.deb --no-check-certificate --directory-prefix=/var/root/migration
-wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/${CFVER}/coreutils_8.32-3_iphoneos-arm.deb --no-check-certificate --directory-prefix=/var/root/migration
-wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/${CFVER}/firmware-sbin_0-1_iphoneos-arm.deb --no-check-certificate --directory-prefix=/var/root/migration
-wget -q http://yaya48.gq/files/migration-files/elucubratustoprocursus/libncurses.6.dylib --no-check-certificate --directory-prefix=/var/root/migration
-dpkg -i /var/root/migration/procursus-keyring_2020.05.09_iphoneos-arm.deb
+rm -rf /tmp/procursus-migration
+mkdir /tmp/procursus-migration
+wget -q http://apt.procurs.us/pool/main/iphoneos-arm64/${CFVER}/procursus-keyring_2020.05.09_iphoneos-arm.deb --no-check-certificate --directory-prefix=/tmp/procursus-migration
+wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/${CFVER}/coreutils_8.32-3_iphoneos-arm.deb --no-check-certificate --directory-prefix=/tmp/procursus-migration
+wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/${CFVER}/firmware-sbin_0-1_iphoneos-arm.deb --no-check-certificate --directory-prefix=/tmp/procursus-migration
+wget -q http://yaya48.gq/files/migration-files/elucubratustoprocursus/libncurses.6.dylib --no-check-certificate --directory-prefix=/tmp/procursus-migration
+dpkg -i /tmp/procursus-migration/procursus-keyring_2020.05.09_iphoneos-arm.deb
 apt update
 apt install xz-utils -y --allow-unauthenticated -u -o APT::Force-LoopBreak=1
 wget -q https://yaya48.gq/files/migration-files/shared/${CFVER}/liblzma.dylib --no-check-certificate --directory-prefix=/usr/local/lib
 wget -q https://yaya48.gq/files/migration-files/shared/${CFVER}/liblzma.5.dylib --no-check-certificate --directory-prefix=/usr/local/lib
 apt install xz-utils -y --allow-unauthenticated -u -o APT::Force-LoopBreak=1
 apt full-upgrade -y --allow-unauthenticated -u -o APT::Force-LoopBreak=1 
-cp /var/root/migration/libncurses.6.dylib /usr/lib
-dpkg -i --force-all /var/root/migration/firmware-sbin_0-1_iphoneos-arm.deb
+cp /tmp/procursus-migration/libncurses.6.dylib /usr/lib
+dpkg -i --force-all /tmp/procursus-migration/firmware-sbin_0-1_iphoneos-arm.deb
 apt full-upgrade -y --allow-unauthenticated -u -o APT::Force-LoopBreak=1
-dpkg -i --force-all /var/root/migration/coreutils_8.32-3_iphoneos-arm.deb
+dpkg -i --force-all /tmp/procursus-migration/coreutils_8.32-3_iphoneos-arm.deb
 dpkg -r apt1.4
 apt update
 apt purge libplist-utils -y libplist3 -y
