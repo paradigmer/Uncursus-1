dd2a8335620792606c5959a3d4714a017498ba07
Yaya4 <34249303+Yaya48@users.noreply.github.com>
2020-09-02 04:27:27 +0200

    Some changes

--- procursus-migration.sh
+++ procursus-migration.sh
@@ -4,9 +4,9 @@ echo You need to run this script as root.
 else
 clear
 echo "Copyright (c) 2020, Yaya4 All rights reserved."
-echo -e "\e[31mUncursus 2.0 Migration Part By Yaya4_4 1.1.2 (Stable)\e[0m"
+echo -e "\e[31mUncursus 2.0 Migration Part By Yaya4_4 1.2 (Stable)\e[0m"
 checkiOSVersion(){
-if command dpkg -s com.bingner.plutil >/dev/null 2>&1 ; then
+echo "Checking iOS Version"
 VER=$(/usr/bin/plutil -key ProductVersion /System/Library/CoreServices/SystemVersion.plist)
 if [[ "${VER%.*}" -ge 12 ]] && [[ "${VER%.*}" -lt 13 ]]; then
 echo "iOS 12 detected, setting the CFVER to 1500"
@@ -24,17 +24,15 @@ else
 echo "Your iOS Version Is Under iOS 12 Or Either Than 13"
 exit 1
 fi
-else
-echo "Apprentely uncursus can't check your're ios version please make sure plutil is installed"
-exit 1
-fi
 }
-echo "Checking iOS Version"
-checkiOSVersion
-echo -e "\e[32mStarting Migration On iOS $VER ....\e[0m"
-COREUTILSVER=8.32-4
+checkDependencies(){
+echo "Checking Dependencies ..."
 apt update
 apt install wget -y --allow-unauthenticated
+}
+ProcursusMigration(){
+echo "Migrating..."
+COREUTILSVER=8.32-4
 rm /etc/apt/sources.list.d/cydia.list
 echo "deb https://apt.procurs.us/ iphoneos-arm64/${CFVER} main" >> /etc/apt/sources.list.d/cydia.list
 rm -rf /tmp/procursus-migration
@@ -45,7 +43,7 @@ dpkg -i /tmp/procursus-migration/procursus-keyring_2020.05.09_iphoneos-arm.deb
 apt update
 apt install libncursesw6 -y
 if [ ! -f "/usr/lib/libncurses.6.dylib" ]; then
-echo "Fixing..."
+echo "Fixing ..."
 ln -s /usr/lib/libncursesw.6.dylib /usr/lib/libncurses.6.dylib
 else
 echo "Nothing To Do!"
@@ -54,15 +52,28 @@ apt install ncurses-bin -y
 apt install xz-utils -y --allow-unauthenticated -u -o APT::Force-LoopBreak=1
 apt dist-upgrade -y --allow-unauthenticated -u -o APT::Force-LoopBreak=1
 dpkg -i --force-all /tmp/procursus-migration/coreutils_${COREUTILSVER}_iphoneos-arm.deb
+}
+MigrationCleanUp(){
+echo "CleaningUp ..."
 dpkg -r apt1.4
 apt update
 apt purge libplist-utils -y libplist3 -y
 apt autoremove -y
 apt install libplist-utils -y libplist++-dev -y libplist++-dev -y libplist++3v5 -y libplist-dev -y libplist3 -y ldid -y
+}
+ProcursusSourcesSetup(){
+echo "Settings Up Procursus Source ..."
 echo "Types: deb" > /etc/apt/sources.list.d/procursus.sources
 echo "URIs: https://apt.procurs.us/" >> /etc/apt/sources.list.d/procursus.sources
 echo "Suites: iphoneos-arm64/${CFVER}" >> /etc/apt/sources.list.d/procursus.sources
 echo "Components: main" >> /etc/apt/sources.list.d/procursus.sources
+}
+checkiOSVersion
+echo -e "\e[32mStarting Migration On iOS $VER ....\e[0m"
+checkDependencies
+ProcursusMigration
+MigrationCleanUp
+ProcursusSourcesSetup
 echo -e "\e[32mMigration Finished!\e[0m"
 echo -e "\e[32mBack to Uncursus Script...\e[0m"
 fi
