7e45ba484c58bf5fd5e054dfab4b0291673425f1
paradigmer <59584564+paradigmer@users.noreply.github.com>
2022-10-28 18:52:28 +0300

    Update procursus-migration.sh

--- procursus-migration.sh
+++ procursus-migration.sh
@@ -28,35 +28,35 @@ else
         mkdir /tmp/procursus-migration
         mkdir /tmp/zstd-support/
         cd /tmp/procursus-migration
-        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/keyring/procursus-keyring_2020.05.09-3_all.deb --no-check-certificate
+        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/keyring/procursus-keyring_2020.05.09-3_all.deb --no-check-certificate  --directory-prefix=/tmp/procursus-migration
         dpkg -i /tmp/procursus-migration/procursus-keyring_2020.05.09-3_all.deb
         apt update 
         cd /tmp/zstd-support/
         apt download libintl8 liblzma5 lz4 xz liblz4-1 xz-utils
-        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/libintl8_0.21-4_iphoneos-arm.deb --no-check-certificate
-        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/liblzma5_5.2.5-3_iphoneos-arm.deb --no-check-certificate
-        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/lz4_1.9.3_iphoneos-arm.deb --no-check-certificate
-        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/xz-utils_5.2.5-3_iphoneos-arm.deb --no-check-certificate
-        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/liblz4-1_1.9.3_iphoneos-arm.deb --no-check-certificate
-        wget -q https://apt.bingner.com/debs/1443.00/xz_5.2.4-4_iphoneos-arm.deb --no-check-certificate
-        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/libzstd1_1.5.2_iphoneos-arm.deb --no-check-certificate
-        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/zstd_1.5.2_iphoneos-arm.deb --no-check-certificate
+        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/libintl8_0.21-4_iphoneos-arm.deb --no-check-certificate --directory-prefix=/tmp/zstd-support/
+        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/liblzma5_5.2.5-3_iphoneos-arm.deb --no-check-certificate --directory-prefix=/tmp/zstd-support/
+        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/lz4_1.9.3_iphoneos-arm.deb --no-check-certificate --directory-prefix=/tmp/zstd-support/
+        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/xz-utils_5.2.5-3_iphoneos-arm.deb --no-check-certificate --directory-prefix=/tmp/zstd-support/
+        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/liblz4-1_1.9.3_iphoneos-arm.deb --no-check-certificate --directory-prefix=/tmp/zstd-support/
+        wget -q https://apt.bingner.com/debs/1443.00/xz_5.2.4-4_iphoneos-arm.deb --no-check-certificate --directory-prefix=/tmp/zstd-support/
+        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/libzstd1_1.5.2_iphoneos-arm.deb --no-check-certificate --directory-prefix=/tmp/zstd-support/
+        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/zstd_1.5.2_iphoneos-arm.deb --no-check-certificate --directory-prefix=/tmp/zstd-support/
         dpkg -i --force-all *.deb
         cd /tmp/procursus-migration
-        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/libzstd1_1.5.2_iphoneos-arm.deb --no-check-certificate
-        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/apt/apt_2.5.2_iphoneos-arm.deb --no-check-certificate
-        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/apt/libapt-pkg6.0_2.5.2_iphoneos-arm.deb --no-check-certificate
-        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/xz-utils_5.2.5-3_iphoneos-arm.deb --no-check-certificate
-        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/liblzma5_5.2.5-3_iphoneos-arm.deb --no-check-certificate
-        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/libncursesw6_6.3-2_iphoneos-arm.deb --no-check-certificate
-        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/ncurses-term_6.2-1_iphoneos-arm.deb --no-check-certificate
-        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/libxxhash0_0.8.1_iphoneos-arm.deb --no-check-certificate
-        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/libxxhash-dev_0.8.1_iphoneos-arm.deb --no-check-certificate
-        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/libgcrypt20_1.9.4_iphoneos-arm.deb --no-check-certificate
-        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/libgpg-error0_1.45_iphoneos-arm.deb --no-check-certificate
-        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/dpkg/dpkg_1.21.9_iphoneos-arm.deb --no-check-certificate       
+        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/libzstd1_1.5.2_iphoneos-arm.deb --no-check-certificate --directory-prefix=/tmp/procursus-migration
+        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/apt/apt_2.5.2_iphoneos-arm.deb --no-check-certificate --directory-prefix=/tmp/procursus-migratio
+        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/apt/libapt-pkg6.0_2.5.2_iphoneos-arm.deb --no-check-certificate --directory-prefix=/tmp/procursus-migratio
+        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/xz-utils_5.2.5-3_iphoneos-arm.deb --no-check-certificate --directory-prefix=/tmp/procursus-migration
+        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/liblzma5_5.2.5-3_iphoneos-arm.deb --no-check-certificate --directory-prefix=/tmp/procursus-migration
+        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/libncursesw6_6.3-2_iphoneos-arm.deb --no-check-certificate --directory-prefix=/tmp/procursus-migration
+        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/ncurses-term_6.2-1_iphoneos-arm.deb --no-check-certificate --directory-prefix=/tmp/procursus-migration
+        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/libxxhash0_0.8.1_iphoneos-arm.deb --no-check-certificate --directory-prefix=/tmp/procursus-migration
+        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/libxxhash-dev_0.8.1_iphoneos-arm.deb --no-check-certificate --directory-prefix=/tmp/procursus-migration
+        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/libgcrypt20_1.9.4_iphoneos-arm.deb --no-check-certificate --directory-prefix=/tmp/procursus-migration
+        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/libgpg-error0_1.45_iphoneos-arm.deb --no-check-certificate --directory-prefix=/tmp/procursus-migration
+        wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/dpkg/dpkg_1.21.9_iphoneos-arm.deb --no-check-certificate        --directory-prefix=/tmp/procursus-migration
         apt download libzstd1 apt libapt-pkg6.0 xz-utils liblzma5 libncursesw6 ncurses-term libxxhash0 libxxhash-dev libgcrypt20 libgpg-error0 dpkg
-        dpkg -i --force-all /tmp/procursus-migration/libncursesw6*.deb
+        dpkg -i --force-all /tmp/procursus-migration/*.deb
         if [ ! -f "/usr/lib/libncurses.6.dylib" ]; then
             echo "Fixing ..."
             ln -s /usr/lib/libncursesw.6.dylib /usr/lib/libncurses.6.dylib
